cb9480a2a1f75e8711787a102f98d73621de1b5c
Bogdan Ceausu <Bogdan.Ceausu@protonmail.com>
2024-11-30 17:54:21 +0200

    up version

--- config/config.go
+++ config/config.go
@@ -19,7 +19,7 @@ var debug = flag.Bool("debug", false, "use -debug arg to log to a file")
 const (
 	ReqTimeout = 10 * time.Second
 
-	defVersion        = "0.3.5"
+	defVersion        = "0.4.1"
 	cfgSubDir         = "sonicRadio"
 	cfgFilename       = "config.json"
 	defHistorySaveMax = 100
