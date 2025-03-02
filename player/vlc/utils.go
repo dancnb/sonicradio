e9eea670a5940f7d700955b850118b3446be78e2
Bogdan Ceausu <Bogdan.Ceausu@protonmail.com>
2025-03-02 14:03:48 +0200

    wip

--- player/vlc/utils.go
+++ player/vlc/utils.go
@@ -3,21 +3,15 @@
 package vlc
 
 import (
-	"context"
 	"errors"
-	"fmt"
-	"log/slog"
-	"net"
-	"os"
-	"os/exec"
-	"slices"
 	"time"
 )
 
 var (
-	baseArgs = []string{"-I", "oldrc", "--rc-fake-tty", "--volume-step", "12.8", "--gain", "1.0", "--no-video", "--rc-unix"}
+	// baseArgs = []string{"-I", "oldrc", "--rc-fake-tty", "--volume-step", "12.8", "--gain", "1.0", "--no-video", "--rc-unix"}
+	// baseArgs = []string{"-I", "rc", "--rc-fake-tty", "--volume-step", "12.8", "--gain", "1.0", "--no-video", "--rc-unix"}
+	// connAddr = "/tmp/vlc%d.sock"
 	// connArg  = "--rc-unix"
-	connAddr = "/tmp/vlc%d.sock"
 
 	socketTimeout    = time.Second * 2
 	socketSleepRetry = time.Millisecond * 10
@@ -33,54 +27,54 @@ func GetBaseCmd() string {
 	return baseCmd
 }
 
-func getConn(ctx context.Context) (net.Conn, error) {
-	sockFile, err := getUnixSocket(ctx)
-	if err != nil {
-		return nil, err
-	}
+// func getConn(ctx context.Context) (net.Conn, error) {
+// 	sockFile, err := getUnixSocket(ctx)
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	start := time.Now()
-loop:
-	for {
-		select {
-		case <-ctx.Done():
-			return nil, ErrCtxCancel
-		case <-time.After(socketTimeout):
-			return nil, ErrSocketFileTimeout
-		default:
-			if _, err := os.Stat(sockFile); os.IsNotExist(err) {
-				time.Sleep(socketSleepRetry)
-			} else {
-				break loop
-			}
-		}
-	}
-	slog.Info(fmt.Sprintf("vlc unix socket file created after %v", time.Since(start)))
+// 	start := time.Now()
+// loop:
+// 	for {
+// 		select {
+// 		case <-ctx.Done():
+// 			return nil, ErrCtxCancel
+// 		case <-time.After(socketTimeout):
+// 			return nil, ErrSocketFileTimeout
+// 		default:
+// 			if _, err := os.Stat(sockFile); os.IsNotExist(err) {
+// 				time.Sleep(socketSleepRetry)
+// 			} else {
+// 				break loop
+// 			}
+// 		}
+// 	}
+// 	slog.Info(fmt.Sprintf("vlc unix socket file created after %v", time.Since(start)))
 
-	var d net.Dialer
-	conn, err := d.DialContext(ctx, "unix", sockFile)
-	return conn, err
-}
+// 	var d net.Dialer
+// 	conn, err := d.DialContext(ctx, "unix", sockFile)
+// 	return conn, err
+// }
 
-func getUnixSocket(ctx context.Context) (string, error) {
-	log := slog.With("method", "vlcCmd")
-	sockFile := fmt.Sprintf(connAddr, os.Getpid())
-	args := slices.Clone(baseArgs)
-	args = append(args, sockFile)
-	cmd := exec.CommandContext(ctx, GetBaseCmd(), args...)
-	if errors.Is(cmd.Err, exec.ErrDot) {
-		cmd.Err = nil
-	} else if cmd.Err != nil {
-		log.Error("vlc cmd error", "error", cmd.Err.Error())
-		return "", cmd.Err
-	}
-	// cmd.Stderr = &bytes.Buffer{}
-	// cmd.Stdin = &bytes.Buffer{}
-	err := cmd.Start()
-	if err != nil {
-		log.Error("vlc cmd start", "error", err)
-		return "", err
-	}
-	log.Info("vlc cmd started", "pid", cmd.Process.Pid)
-	return sockFile, nil
-}
+// func getUnixSocket(ctx context.Context) (string, error) {
+// 	log := slog.With("method", "vlcCmd")
+// 	sockFile := fmt.Sprintf(connAddr, os.Getpid())
+// 	args := slices.Clone(baseArgs)
+// 	args = append(args, sockFile)
+// 	cmd := exec.CommandContext(ctx, GetBaseCmd(), args...)
+// 	if errors.Is(cmd.Err, exec.ErrDot) {
+// 		cmd.Err = nil
+// 	} else if cmd.Err != nil {
+// 		log.Error("vlc cmd error", "error", cmd.Err.Error())
+// 		return "", cmd.Err
+// 	}
+// 	// cmd.Stderr = &bytes.Buffer{}
+// 	// cmd.Stdin = &bytes.Buffer{}
+// 	err := cmd.Start()
+// 	if err != nil {
+// 		log.Error("vlc cmd start", "error", err)
+// 		return "", err
+// 	}
+// 	log.Info("vlc cmd started", "pid", cmd.Process.Pid)
+// 	return sockFile, nil
+// }
--- player/vlc/vlc.go
+++ player/vlc/vlc.go
@@ -1,10 +1,15 @@
 package vlc
 
 import (
+	"bufio"
 	"context"
+	"errors"
 	"fmt"
 	"log/slog"
 	"net"
+	"os/exec"
+	"slices"
+	"strings"
 	"time"
 
 	"github.com/dancnb/sonicradio/config"
@@ -12,6 +17,8 @@ import (
 )
 
 // var baseArgs = []string{"-I rc", "--no-video", "--volume-step 12.8", "--gain 1.0"}
+// var baseArgs = []string{"-I", "rc", "--rc-fake-tty", "--volume-step", "12.8", "--gain", "1.0", "--no-video", "--rc-host"}
+var baseArgs = []string{"-I", "rc", "--volume-step", "12.8", "--gain", "1.0", "--no-video", "--rc-host"}
 
 type Vlc struct {
 	conn net.Conn
@@ -31,31 +38,97 @@ const (
 	playbackTime
 	seek
 	quit
+	shutdown
 )
 
 var cmds = map[vlcRcCmd]string{
-	add:      `add %s`,
-	play:     `play`,
-	stop:     `stop`,
-	pause:    `pause`,
-	unpause:  `pause`,
-	volume:   `volume %f`,
-	metadata: `info`,
-	// mediaTitle:   `["get_property", "media-title"]`,
-	playbackTime: `get_time`,
-	seek:         `seek %d`,
-	quit:         `quit`,
+	add:          "add %s\n",
+	play:         "play\n",
+	stop:         "stop\n",
+	pause:        "pause\n",
+	unpause:      "pause\n",
+	volume:       "volume %f\n",
+	metadata:     "info\n",
+	playbackTime: "get_time\n",
+	seek:         "seek %d\n",
+	// quit:         "quit\n", // not good
+	shutdown: "shutdown\n",
 }
 
 func NewVlc(ctx context.Context) (*Vlc, error) {
 	p := &Vlc{}
 
-	conn, err := getConn(ctx)
+	port, err := p.getAvailablePort()
 	if err != nil {
 		return nil, err
 	}
-	p.conn = conn
+	addr := fmt.Sprintf("localhost:%s", port)
+	_, err = p.vlcCmd(ctx, addr)
+	if err != nil {
+		return nil, err
+	}
+	// mpv.cmd = cmd
+
+	start := time.Now()
+loop:
+	for {
+		select {
+		case <-ctx.Done():
+			return nil, ErrCtxCancel
+		case <-time.After(socketTimeout):
+			return nil, ErrSocketFileTimeout
+		default:
+			conn, err := getConn(ctx, addr)
+			if err != nil {
+				time.Sleep(socketSleepRetry)
+			}
+			p.conn = conn
+			break loop
+		}
+	}
+	slog.Info(fmt.Sprintf("vlc tcp connection created after %v", time.Since(start)))
+
 	return p, nil
+}
+
+func (v *Vlc) getAvailablePort() (string, error) {
+	l, err := net.Listen("tcp", ":0")
+	if err != nil {
+		return "", err
+	}
+	defer func() { _ = l.Close() }()
+	addr := l.Addr().String()
+	_, p, err := net.SplitHostPort(addr)
+	if err != nil {
+		return "", err
+	}
+	return p, nil
+}
+
+func (v *Vlc) vlcCmd(ctx context.Context, addr string) (*exec.Cmd, error) {
+	log := slog.With("method", "vlcCmd")
+	args := slices.Clone(baseArgs)
+	args = append(args, addr)
+	cmd := exec.CommandContext(ctx, GetBaseCmd(), args...)
+	if errors.Is(cmd.Err, exec.ErrDot) {
+		cmd.Err = nil
+	} else if cmd.Err != nil {
+		log.Error("vlc cmd error", "error", cmd.Err.Error())
+		return nil, cmd.Err
+	}
+	err := cmd.Start()
+	if err != nil {
+		log.Error("vlc cmd start", "error", err)
+		return nil, err
+	}
+	log.Info("vlc cmd started", "pid", cmd.Process.Pid)
+	return cmd, nil
+}
+
+func getConn(ctx context.Context, addr string) (net.Conn, error) {
+	var d net.Dialer
+	conn, err := d.DialContext(ctx, "tcp", addr)
+	return conn, err
 }
 
 func (v *Vlc) GetType() config.PlayerType {
@@ -68,11 +141,11 @@ func (v *Vlc) Play(url string) error {
 	if err != nil {
 		return err
 	}
-	cmd = cmds[play]
-	_, err = v.doRequest(cmd)
-	if err != nil {
-		return err
-	}
+	// cmd = cmds[play]
+	// _, err = v.doRequest(cmd)
+	// if err != nil {
+	// 	return err
+	// }
 	return nil
 }
 
@@ -98,6 +171,7 @@ func (v *Vlc) Seek(amtSec int) *model.Metadata {
 	return nil
 }
 
+// TODO: also kill command?
 func (v *Vlc) Close() (err error) {
 	log := slog.With("method", "Vlc.Close")
 	log.Info("stopping")
@@ -113,8 +187,8 @@ func (v *Vlc) Close() (err error) {
 		}
 	}()
 
-	quitCmd := cmds[quit]
-	_, err = v.doRequest(quitCmd)
+	cmd := cmds[shutdown]
+	_, err = v.doRequest(cmd)
 	return err
 }
 
@@ -128,14 +202,34 @@ func (v *Vlc) doRequest(cmd string) (string, error) {
 		return "", fmt.Errorf("vlc write err: %w", err)
 	}
 
-	v.conn.SetDeadline(time.Now().Add(config.MpvIpcConnTimeout))
-	buff := make([]byte, 4096)
-	_, err = v.conn.Read(buff)
-	if err != nil {
-		return "", fmt.Errorf("vlc read err: %w", err)
-	}
-	res := string(buff)
-	log.Debug("vlc", "response", res)
+	// v.conn.SetDeadline(time.Now().Add(config.VlcConnTimeout))
+	// buff := make([]byte, 4096)
+	// _, err = v.conn.Read(buff)
+	// if err != nil {
+	// 	return "", fmt.Errorf("vlc read err: %w", err)
+	// }
+	// res := string(buff)
+	// log.Debug("vlc", "response", res)
+	// return res, nil
 
-	return res, nil
+	// scanner
+	//
+	scanner := bufio.NewScanner(v.conn)
+	v.conn.SetDeadline(time.Now().Add(config.VlcConnTimeout))
+	var res strings.Builder
+	for scanner.Scan() {
+		l := scanner.Text()
+		res.WriteString(l)
+		res.WriteString("\n")
+		log.Info(fmt.Sprintf("resp=%s", l))
+		if l == "> " {
+			break
+		}
+		v.conn.SetDeadline(time.Now().Add(config.VlcConnTimeout))
+	}
+	// if err := scanner.Err(); err != nil {
+	// 	return "", fmt.Errorf("scanner error: %w", err)
+	// }
+	return res.String(), nil
+
 }
