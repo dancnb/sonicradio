8182143c4928010dd0d53186926b64093e5b7e3f
Bogdan Ceausu <Bogdan.Ceausu@protonmail.com>
2025-03-02 16:14:12 +0200

    vlc unix

--- player/vlc/utils.go
+++ player/vlc/utils.go
@@ -4,7 +4,7 @@ package vlc
 
 // var baseArgs = []string{"-I rc", "--no-video", "--volume-step 12.8", "--gain 1.0"}
 // var baseArgs = []string{"-I", "rc", "--rc-fake-tty", "--volume-step", "12.8", "--gain", "1.0", "--no-video", "--rc-host"}
-var baseArgs = []string{"-I", "rc", "--volume-step", "12.8", "--gain", "1.0", "--no-video", "--rc-host"}
+var baseArgs = []string{"-I", "rc", "--rc-fake-tty", "--volume-step", "12.8", "--gain", "1.0", "--no-video", "--rc-host"}
 
 const baseCmd = "vlc"
 
