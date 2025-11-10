4f0773c0babee494ea1b11b19cd0018998c098bc
Bogdan Ceausu <n.bogdan.ceausu@gmail.com>
2025-11-10 18:24:29 +0200

    up dev version

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ import (
 )
 
 var (
-	version = "0.8.5"
+	version = "0.8.8"
 
 	cpuProfile  = flag.String("cpuprofile", "", "-cpuprofile=<filename>")
 	memProfile  = flag.String("memprofile", "", "-memprofile=<filename>")
